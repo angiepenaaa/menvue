69732c3eeddc147d64ff18d4c3454229229e1687
angiepenaaa <angeliepena21@gmail.com>
2025-05-28 19:49:19 -0400

    Add more restaurants and menu items

--- src/data/restaurants.ts
+++ src/data/restaurants.ts
@@ -1,6 +1,7 @@
 import { Restaurant } from '../types';
 
 export const restaurants: Restaurant[] = [
+  // Existing restaurants
   {
     id: '1',
     name: 'First Watch',
@@ -8,7 +9,8 @@ export const restaurants: Restaurant[] = [
     rating: 4.6,
     distance: '0.8 mi',
     deliveryTime: '20-30 min',
-    image: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg'
+    image: 'https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg',
+    location: 'Brandon, FL'
   },
   {
     id: '2',
@@ -17,7 +19,8 @@ export const restaurants: Restaurant[] = [
     rating: 4.5,
     distance: '1.1 mi',
     deliveryTime: '15-25 min',
-    image: 'https://images.pexels.com/photos/5945754/pexels-photo-5945754.jpeg'
+    image: 'https://images.pexels.com/photos/5945754/pexels-photo-5945754.jpeg',
+    location: 'Brandon, FL'
   },
   {
     id: '3',
@@ -26,24 +29,58 @@ export const restaurants: Restaurant[] = [
     rating: 4.3,
     distance: '1.4 mi',
     deliveryTime: '20-35 min',
-    image: 'https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg'
+    image: 'https://images.pexels.com/photos/1640770/pexels-photo-1640770.jpeg',
+    location: 'Brandon, FL'
   },
+  // New restaurants
   {
-    id: '4',
-    name: 'Tropical Smoothie Cafe',
-    cuisine: 'Smoothies & Bowls',
-    rating: 4.4,
-    distance: '0.6 mi',
+    id: '6',
+    name: 'Sweetgreen',
+    cuisine: 'Salads & Bowls',
+    rating: 4.7,
+    distance: '1.2 mi',
     deliveryTime: '15-25 min',
-    image: 'https://images.pexels.com/photos/434295/pexels-photo-434295.jpeg'
+    image: 'https://images.pexels.com/photos/1095550/pexels-photo-1095550.jpeg',
+    location: 'Brandon, FL'
   },
   {
-    id: '5',
-    name: 'Panera Bread',
-    cuisine: 'Bakery & Cafe',
-    rating: 4.2,
-    distance: '1.0 mi',
-    deliveryTime: '20-30 min',
-    image: 'https://images.pexels.com/photos/1211887/pexels-photo-1211887.jpeg'
+    id: '7',
+    name: 'True Food Kitchen',
+    cuisine: 'Health Food',
+    rating: 4.5,
+    distance: '2.1 mi',
+    deliveryTime: '25-40 min',
+    image: 'https://images.pexels.com/photos/361184/asparagus-steak-veal-steak-veal-361184.jpeg',
+    location: 'Brandon, FL'
+  },
+  {
+    id: '8',
+    name: 'Protein Bar & Kitchen',
+    cuisine: 'Health Food',
+    rating: 4.4,
+    distance: '0.9 mi',
+    deliveryTime: '15-25 min',
+    image: 'https://images.pexels.com/photos/1279330/pexels-photo-1279330.jpeg',
+    location: 'Brandon, FL'
+  },
+  {
+    id: '9',
+    name: 'Flower Child',
+    cuisine: 'Healthy',
+    rating: 4.6,
+    distance: '1.7 mi',
+    deliveryTime: '20-35 min',
+    image: 'https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg',
+    location: 'Brandon, FL'
+  },
+  {
+    id: '10',
+    name: 'Just Salad',
+    cuisine: 'Salads',
+    rating: 4.3,
+    distance: '1.3 mi',
+    deliveryTime: '15-25 min',
+    image: 'https://images.pexels.com/photos/1059905/pexels-photo-1059905.jpeg',
+    location: 'Brandon, FL'
   }
 ];
