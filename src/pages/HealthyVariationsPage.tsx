daa1398fd00be0ea4b287a1785d39e64900f2ecd
angiepenaaa <angeliepena21@gmail.com>
2025-05-30 14:48:38 -0400

    Updated HealthyVariationsPage.tsx

--- src/pages/HealthyVariationsPage.tsx
+++ src/pages/HealthyVariationsPage.tsx
@@ -100,7 +100,7 @@ const HealthyVariationsPage: React.FC = () => {
               >
                 {isAdvisorLoading ? (
                   <>
-                    <Loader2 className="animate-spin" size={18} />
+                    <Loader2 className="animate-spin\" size={18} />
                     <span>Generating suggestions...</span>
                   </>
                 ) : (
